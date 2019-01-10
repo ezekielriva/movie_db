096c73a5a9374c2aa30c630a22c96ba1410cd5c0
Ezekiel <ezequiel.lichardi@ring.com>
2019-01-10 01:00:29 -0300

    Fix migration

--- db/migrate/20190110001035_create_auth_tokens.rb
+++ db/migrate/20190110001035_create_auth_tokens.rb
@@ -2,7 +2,7 @@ class CreateAuthTokens < ActiveRecord::Migration[5.2]
   def change
     create_table :auth_tokens do |t|
       t.string :token
-      t.ate_time :iat
+      t.datetime :iat
       t.datetime :exp
     end
   end
